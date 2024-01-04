7090715e113107384521b5cfb28b920dbe83f2f9
imopbuilder <opbuilder9@gmail.com>
2024-01-04 19:20:31 +0530

    enhance: enhance Devices in the MediaDevices component in home page

--- src/components/pages/home/client.tsx
+++ src/components/pages/home/client.tsx
@@ -26,51 +26,6 @@ export function Webrtc() {
 }
 
 function MediaDevices() {
-	const { devices, selectedDevices, streamLoading } = useAppSelector((state) => state.mediaDeviceSlice);
-
-	function SelectedDeviceLabel(props: { kind: MediaDeviceKind }) {
-		if (streamLoading) return <Skeleton className='h-3 mt-1 w-48 rounded-sm' />;
-
-		const deviceLabel = devices.find((device) => device.deviceId === selectedDevices[props.kind])?.label;
-
-		if (deviceLabel) return <span className='text-xs text-muted-foreground font-medium pl-0.5'>{deviceLabel}</span>;
-
-		return <span className='text-xs text-muted-foreground font-medium pl-0.5'>- No device found -</span>;
-	}
-
-	function Devices({ title, kind }: { title: string; kind: MediaDeviceKind }) {
-		return (
-			<AccordionItem value={kind} className='mb-5 last:mb-0 border-none'>
-				<AccordionTrigger className='bg-muted px-4 py-3 rounded-lg font-semibold hover:no-underline hover:bg-muted/80'>
-					<span className='flex items-start justify-center flex-col gap-1'>
-						<span>{title}</span>
-						<SelectedDeviceLabel kind={kind} />
-					</span>
-				</AccordionTrigger>
-				<AccordionContent className='pt-4 px-4 border-b'>
-					<RadioGroup value={selectedDevices[kind] ?? undefined} onValueChange={(val) => console.log(val)}>
-						{devices
-							.filter((device) => device.kind === kind)
-							.map((device, index) => {
-								return (
-									<div
-										key={device.deviceId}
-										className={cn(
-											'flex items-center gap-2 py-1 text-sm',
-											selectedDevices[kind] === device.deviceId ? 'text-foreground' : 'text-muted-foreground',
-										)}
-									>
-										<RadioGroupItem value={device.deviceId} id={`r${index}`} />
-										<Label htmlFor={`r${index}`}>{device.label}</Label>
-									</div>
-								);
-							})}
-					</RadioGroup>
-				</AccordionContent>
-			</AccordionItem>
-		);
-	}
-
 	return (
 		<div className='w-full'>
 			<Accordion type='multiple'>
@@ -84,5 +39,48 @@ function MediaDevices() {
 				<Devices title='Speaker' kind='audiooutput' />
 			</Accordion>
 		</div>
+	);
+}
+
+function Devices({ title, kind }: { title: string; kind: MediaDeviceKind }) {
+	const { devices, selectedDevices, streamLoading } = useAppSelector((state) => state.mediaDeviceSlice);
+
+	function SelectedDeviceLabel(props: { kind: MediaDeviceKind }) {
+		if (streamLoading) return <Skeleton className='h-3 mt-1 w-48 rounded-sm' />;
+
+		const deviceLabel = devices.find((device) => device.deviceId === selectedDevices[props.kind])?.label;
+		if (deviceLabel) return <span className='text-xs text-muted-foreground font-medium pl-0.5'>{deviceLabel}</span>;
+		return <span className='text-xs text-muted-foreground font-medium pl-0.5'>- No device found -</span>;
+	}
+
+	return (
+		<AccordionItem value={kind} className='mb-5 last:mb-0 border-none'>
+			<AccordionTrigger className='bg-muted px-4 py-3 rounded-lg font-semibold hover:no-underline hover:bg-muted/80'>
+				<span className='flex items-start justify-center flex-col gap-1'>
+					<span>{title}</span>
+					<SelectedDeviceLabel kind={kind} />
+				</span>
+			</AccordionTrigger>
+			<AccordionContent className='pt-4 px-4 border-b'>
+				<RadioGroup value={selectedDevices[kind] ?? undefined} onValueChange={(val) => console.log(val)}>
+					{devices
+						.filter((device) => device.kind === kind)
+						.map((device, index) => {
+							return (
+								<div
+									key={device.deviceId}
+									className={cn(
+										'flex items-center gap-2 py-1 text-sm',
+										selectedDevices[kind] === device.deviceId ? 'text-foreground' : 'text-muted-foreground',
+									)}
+								>
+									<RadioGroupItem value={device.deviceId} id={`r${index}`} />
+									<Label htmlFor={`r${index}`}>{device.label}</Label>
+								</div>
+							);
+						})}
+				</RadioGroup>
+			</AccordionContent>
+		</AccordionItem>
 	);
 }
